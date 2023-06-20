f8e837b68fb0858a5c79bf960eda5143fbc13590
Strykeros <edijsz@tutanota.com>
2023-06-20 22:25:57 +0300

    first commit

---
+++ Game.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpaceInvaders2020
+{
+    public partial class Game : Form
+    {
+        private Spaceship spaceship = null;
+        private List<Enemy> enemies = new List<Enemy>();        
+        private Timer mainTimer = null;
+
+        public Game()
+        {
+            InitializeComponent();
+            InitializeGame();
+            InitializeMainTimer();
+        }
+
+        private void InitializeGame()
+        {
+            this.KeyDown += Game_KeyDown;
+            this.BackColor = Color.Black;
+            AddSpaceshipToGame();
+            AddEnemyToGame(3, 3);
+        }
+
+        private void AddSpaceshipToGame()
+        {
+            spaceship = new Spaceship(this);
+            spaceship.FireCooldown = 500;
+            this.Controls.Add(spaceship);
+            spaceship.Left = 150;
+            spaceship.Top = ClientRectangle.Height - spaceship.Height;
+        }
+
+        private void AddEnemyToGame(int rows, int columns)
+        {
+            Enemy enemy = null;
+            
+            for(int rowCounter = 0; rowCounter < rows; rowCounter++)
+            {
+                for (int colCounter = 0; colCounter < columns; colCounter++)
+                {
+                    enemy = new Enemy(this);
+                    enemy.Left = 20 + 60 * colCounter;
+                    enemy.Top = 20 + 60 * rowCounter;
+                    this.Controls.Add(enemy);
+                    enemies.Add(enemy);
+                }
+            }
+        }
+
+        private void Game_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Space)
+            {
+                spaceship.Fire();
+            }
+            else if(e.KeyCode == Keys.A)
+            {
+                spaceship.MoveLeft();                
+            }
+            else if(e.KeyCode == Keys.D)
+            {
+                spaceship.MoveRight();                
+            }
+            else if(e.KeyCode == Keys.S)
+            {
+                spaceship.MoveStop();                
+            }
+        }
+
+        private void InitializeMainTimer()
+        {
+            mainTimer = new Timer();
+            mainTimer.Interval = 10;
+            mainTimer.Tick += MainTimer_Tick;
+            mainTimer.Start();
+        }
+
+        private void MainTimer_Tick(object sender, EventArgs e)
+        {
+            CheckBulletEnemyCollision();
+        }
+
+        private void CheckBulletEnemyCollision()
+        {
+            foreach (var bullet in spaceship.bullets)
+            {
+                foreach (var enemy in enemies)
+                {
+                    if (bullet.Bounds.IntersectsWith(enemy.Bounds))
+                    {
+                        enemy.Explode();
+
+                        this.Controls.Remove(bullet);
+                        bullet.Dispose();
+
+                        //this is a temporary solution                        
+                        bullet.Top = 0;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Spaceship.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using SpaceInvaders2020.Properties;
+
+namespace SpaceInvaders2020
+{
+    class Spaceship : PictureBox
+    {
+        public int FireCooldown { get; set; } = 1000;
+
+        public int HorVelocity { get; set; } = 0;
+
+        public List<Bullet> bullets = new List<Bullet>();
+
+        private bool canFire = true;
+        private int imageCount = 0;
+
+        private Game game = null;
+        private Timer timerCooldown = null;
+        private Timer timerMove = null;
+        private Timer timerAnimate = null;
+
+        public Spaceship(Game gameForm)
+        {
+            game = gameForm;
+            InitializeSpaceship();
+            InitializeTimerMove();
+            InitializeTimerAnimate();
+        }
+
+        private void InitializeSpaceship()
+        {
+            this.Height = 100;
+            this.Width = 60;
+            this.BackColor = Color.Transparent;
+            this.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            string pictureName = "rocket_on_000";
+            this.Image = (Image)Resources.ResourceManager.GetObject(pictureName);
+        }
+
+        public void Fire()
+        {
+            if (!canFire) return;
+            
+            Bullet bullet = new Bullet();
+            bullet.Left = this.Left + 30;
+            bullet.Top = this.Top - bullet.Height;
+            game.Controls.Add(bullet);
+            bullets.Add(bullet);
+            canFire = false;
+            InitializeTimerCooldown();
+        }
+
+        private void InitializeTimerCooldown()
+        {
+            timerCooldown = new Timer();
+            timerCooldown.Interval = FireCooldown;
+            timerCooldown.Tick += TimerCooldown_Tick;
+            timerCooldown.Start();
+        }
+
+        private void TimerCooldown_Tick(object sender, EventArgs e)
+        {
+            canFire = true;
+            timerCooldown.Stop();
+        }
+
+        private void InitializeTimerMove()
+        {
+            timerMove = new Timer();
+            timerMove.Interval = 10;
+            timerMove.Tick += TimerMove_Tick;
+            timerMove.Start();
+        }
+
+        private void TimerMove_Tick(object sender, EventArgs e)
+        {            
+            this.Left += this.HorVelocity;
+            CheckSpaceshipLocation();
+        }
+
+        private void CheckSpaceshipLocation()
+        {
+            if(this.Left <= 0)
+            {
+                this.HorVelocity = -this.HorVelocity;
+            }
+            else if(this.Left + this.Width >= game.ClientRectangle.Width)
+            {
+                this.HorVelocity = -this.HorVelocity;
+            }
+        }
+
+        public void MoveRight()
+        {
+            this.HorVelocity = 2;
+        }
+        public void MoveLeft()
+        {
+            this.HorVelocity = -2;
+        }
+        public void MoveStop()
+        {
+            this.HorVelocity = 0;
+        }
+
+        private void InitializeTimerAnimate()
+        {
+            timerAnimate = new Timer();
+            timerAnimate.Interval = 80;
+            timerAnimate.Tick += TimerAnimate_Tick;
+            timerAnimate.Start();
+        }
+
+        private void TimerAnimate_Tick(object sender, EventArgs e)
+        {
+            SpaceshipAnimateRotation();
+        }
+
+        private void SpaceshipAnimateRotation()
+        {
+            string imageName = "rocket_off_" + imageCount.ToString("000");
+            this.Image = (Image)Resources.ResourceManager.GetObject(imageName);
+            imageCount += 1;
+            if (imageCount > 3) imageCount = 0;
+        }
+    }
+}
